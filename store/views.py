beb558aa50339d6ccf049f049273cfab01ef7221
esparsh287 <esparsh.tamrakar18@gmail.com>
2025-08-08 10:43:58 +0545

    first commit

---
+++ store/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import redirect, render
+from .models import Product
+from django.contrib.auth import login,logout,authenticate
+from django.contrib import messages
+
+
+def home(request):
+  products=Product.objects.all()
+  return render(request, 'home.html',{'products':products})
+
+
+def about(request):
+  return render(request, 'about.html')
+
+
+def login_user(request):
+  if request.method=="POST":
+    username=request.POST['username']
+    password=request.POST['password']
+    user= authenticate(request, username=username, password=password)
+    if user is not None:
+      login(request, user)
+      messages.success(request, ("You have been logged in !!!"))
+      return redirect('home')
+
+    else:
+      messages.success(request, ("Please Enter Correct Credentials"))
+      return redirect('login')
+  return render(request, 'login.html', {})
+
+
+def logout_user(request):
+  logout(request)
+  messages.success(request, ("You have been logged out!!!"))
+  return redirect('login')

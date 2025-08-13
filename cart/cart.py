0b560a9ea94d447fcc7bbd170d34bed1d2d3a581
esparsh287 <esparsh.tamrakar18@gmail.com>
2025-08-13 10:23:46 +0545

    cart added

--- cart/cart.py
+++ cart/cart.py
@@ -1,4 +1,4 @@
-
+from store.models import Product
 
 class Cart():
   def __init__(self, request):
@@ -9,3 +9,31 @@ class Cart():
       cart=self.session['session_key']={}
 
     self.cart=cart
+
+
+  def add(self, product, quantity):
+    product_id= str(product.id)
+    product_qty=str(quantity)
+
+    if product_id in self.cart:
+      pass
+    else:
+      # self.cart[product_id]= {'price': str(product.price)}
+      self.cart[product_id]=int(product_qty)
+
+    self.session.modified= True
+
+
+  def __len__(self):
+    return len(self.cart)
+  
+
+  def get_prods(self):
+    product_ids=self.cart.keys()
+    products= Product.objects.filter(id__in= product_ids)
+    return products
+  
+
+  def get_quants(self):
+    quantities=self.cart
+    return quantities
